c2a44fb8b92dce9dc5596393fd4990548c755c85
Dmitry <dmitry.alex.sergeev@gmail.com>
2026-03-31 19:43:40 +0200

    added history log and file output

--- First_test_robot_control.py
+++ First_test_robot_control.py
@@ -1,7 +1,31 @@
+from sys import argv
+from os.path import exists
+
+script, input_file = argv
+
+print(f"Does the log file exists? {exists(input_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
 #starting coordinates of a robot
 x = 0
 y = 0
-print("Robot position:", "x:", x, "y:", y)
+
+#log for history
+history_log = []
+
+def update_log():
+    global x, y, history_log
+    robot_position = f"x: {x}, y: {y}"
+
+    #writing to a file
+    log_file = open(input_file, 'a')
+    log_file.write(robot_position + '\n')
+    log_file.close()
+
+    # writing to memory
+    history_log.append(robot_position) 
+
+print("Robot position:", f"x: {x}, y: {y}")
+
 #entering a command to move: up, down, left, right
 
 #infinite loop
@@ -9,29 +33,43 @@ while True:
     command = input("Enter command: ") # input moves us into an infitine loop
     
     if command == "quit":
-        quit()
+        open(input_file, 'w').close()
+        break
+
     elif command == "up":
-        y = y + 1
+        y += 1
+        update_log()
+
     elif command == "down":
-        y = y - 1
+        y -= 1
+        update_log()
+
     elif command == "left":
-        x = x - 1
+        x -= 1
+        update_log()
+
     elif command == "right":
-        x = x + 1
+        x += 1
+        update_log()
+
+    elif command == "history":
+        for entry in history_log:
+            print(entry)
+
     else:
         print("Unknown command")
     #making a range
     if x > 5:
-        x = x - 1
+        x -= 1
         print("out of range")
     elif x < -5:
-        x = x + 1
+        x += 1
         print("out of range")
     elif y > 5:
-        y = y - 1
+        y -= 1
         print("out of range")
     elif y < -5:
-        y = y + 1
+        y += 1
         print("out of range")
     else:
-        print("Robot position:", "x:", x, "y:", y)
+        print("Robot position:", f"x: {x}, y: {y}")
