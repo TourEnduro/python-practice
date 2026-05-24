eb171293e408b1bc71fe44af0d450625b9be52e8
Dmitry <dmitry.alex.sergeev@gmail.com>
2026-05-24 14:23:35 +0200

    Exercise 48 - lexicon scanner with tests

---
+++ projects/ex48/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from destutils.core import setup
+
+config = {
+    'description': 'My first test project',
+    'author': 'Dmitry',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'dmitry.alex.sergeev@gmail.com',
+    'version': '0.1',
+    'packages': ['ex48'],
+    'scripts': [],
+    'name': 'ex48'
+}
+
+setup(**config)
